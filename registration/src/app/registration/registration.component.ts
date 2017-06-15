550ca9c20c9228c3365b4c01d03071e8082b55c6
dusty-g <dustygalindo@gmail.com>
2017-06-14 19:03:14 -0700

    Registration

---
+++ registration/src/app/registration/registration.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from "./../user"
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+  users: User[] = []
+  user1: User = new User
+  submitIsActive = false
+  displayResult = false
+  displayErrors = false
+  constructor() { 
+    
+  }
+
+  ngOnInit() {
+    
+  }
+  
+  onSubmit(formData){
+    if(formData.valid){
+      console.log("valid form")
+      
+      this.user1 = formData.form.value
+      console.log(this.user1.luck)
+      if (this.user1.password != this.user1.password_confirmation){
+        this.displayErrors = true
+        return false
+      }
+      // this.user1.luck = !!this.user1.luck
+
+      this.displayResult = true
+
+      
+      return false
+
+    }else{
+      console.log(formData)
+      this.displayErrors = true
+      this.displayResult = false
+
+      return false
+    }
+  }
+}
---
+++ registration/src/app/user.ts
@@ -0,0 +1,12 @@
+export class User {
+  first_name: string
+  last_name: string
+  email: string
+  password: string
+  password_confirmation: string
+  address: string
+  apt_number: string
+  city: string
+  state: string
+  luck: boolean
+}
